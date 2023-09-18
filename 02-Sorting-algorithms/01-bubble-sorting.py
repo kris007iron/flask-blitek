a032ca393cb29b0d4e1b235969fa75929ad54e5f
kris007iron <kris007.iron@gmail.com>
2023-09-18 16:19:31 +0200

    bubble sorting

--- 02-Sorting-algorithms/01-bubble-sorting.py
+++ 02-Sorting-algorithms/01-bubble-sorting.py
@@ -1,16 +1,22 @@
 numbers = [36, 67, 84, 76, 43, 64, 95, 100, 19, 80, 62, 61, 89, 53, 79, 74, 51, 17, 29, 72]
 
 
-def bubble_sorting(elements):
-    """
-      Bubble sorting functions
-      :param elements:
-      :return:
-      """
-    for i in range(len(elements)):
-        for j in range(len(elements) - i - 1):
-            elements[j + 1], elements[j] = elements[j], elements[j + 1]
-    return elements
+def bubble_sort(array):
+    # loop to access each array element
+    for i in range(len(array)):
+
+        # loop to compare array elements
+        for j in range(0, len(array) - i - 1):
+
+            # compare two adjacent elements
+            # change > to < to sort in descending order
+            if array[j] > array[j + 1]:
+                # swapping elements if elements
+                # are not in the intended order
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+    return array
 
 
-print(bubble_sorting(numbers))
+print(bubble_sort(numbers))
