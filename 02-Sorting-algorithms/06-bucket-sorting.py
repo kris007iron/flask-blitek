80c617032c3d3d3f1ab2bcd6ef9cf18a8c1fe729
kris007iron <kris007.iron@gmail.com>
2023-10-02 16:22:37 +0200

    bucket sorting for float

--- 02-Sorting-algorithms/06-bucket-sorting.py
+++ 02-Sorting-algorithms/06-bucket-sorting.py
@@ -27,3 +27,27 @@ def bucket_sorting(arr):
 print(bucket_sorting(data))
 
 
+# bucket sorting algorithm for float numbers
+
+def bucket_sorting_float(arr):
+    # create a new array
+    new_arr = []
+    # loop through the array
+    for i in range(len(arr)):
+        # append the value of the array at the current index to the new array
+        new_arr.append(arr[i])
+    # loop through the new array
+    for i in range(len(new_arr)):
+        # loop through the new array
+        for j in range(len(new_arr)):
+            # if the value of the new array at the current index is greater than the value of the new array at the
+            # next index
+            if new_arr[i] < new_arr[j]:
+                # swap the values
+                new_arr[i], new_arr[j] = new_arr[j], new_arr[i]
+    # return the new array
+    return new_arr
+
+
+print(bucket_sorting_float([0.1, 0.2, 0.3, 0.69, 0.5, 0.34]))
+
