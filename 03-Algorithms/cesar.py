eb32e0440feda07dc490b8a1e5c0bf984ecc1602
kris007iron <kris007.iron@gmail.com>
2024-01-15 16:33:48 +0100

    propably an error

--- 03-Algorithms/cesar.py
+++ 03-Algorithms/cesar.py
@@ -1,2 +1,15 @@
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z']
+message = input('Enter a message: ').lower()
+offset = int(input('Enter an offset: '))
+encrypted_message = ''
+
+for letter in message:
+    if letter in letters:
+        position = letters.index(letter)
+        new_position = (position + offset) % len(letters)
+        encrypted_message += letters[new_position]
+    else:
+        encrypted_message += letter
+
+print(encrypted_message)
