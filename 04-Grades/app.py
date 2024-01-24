5577fd9c8070c5363e1bad924a5bd8f2ac7a99b6
kris007iron <kris007.iron@gmail.com>
2024-01-24 15:19:21 +0100

    register login and exceptions, workin on dashboard.html and add_user in dashboard for admin

--- 04-Grades/app.py
+++ 04-Grades/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, flash
 from flask_bs4 import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired
 from flask_moment import Moment
 from datetime import datetime
@@ -14,7 +14,6 @@ app.config['SECRET_KEY'] = 'ghjghj^&*GHJ%^&*567867899iuyhgfgu%^&*IJHN'
 moment = Moment(app)
 date = datetime.now()
 
-
 class LoginForm(FlaskForm):
     """
     Formularz logowania
@@ -23,121 +22,141 @@ class LoginForm(FlaskForm):
     userPass = PasswordField('Hasło:', validators=[DataRequired()])
     submit = SubmitField('Zaloguj')
 
-
-class Grade(FlaskForm):
-    subject = StringField('Nazwa przedmiotu:', validators=[DataRequired()])
-    term = RadioField("Wybierz semestr", choices=[("term1", "Semestr 1"), ("term2", "Semestr 2")])
-    category = SelectField('Kategoria:',
-                           choices=[('answer', 'Odpowiedź'), ('quiz', 'Kartkówka'), ('test', 'Sprawdzian')])
-    grade = SelectField('Ocena:',
-                        choices=[(6, "Celujący"), (5, "Bardzo dobry"), (4, "Dobry"),
-                                 (3, "Dostateczny"), (2, "Dopuszczający"), (1, "Niedostateczny")])
+class Subject(FlaskForm):
+    """formularz dodawania przedmiotu"""
+    subject = StringField('Nazwa przedmiotu', validators=[DataRequired()])
     submit = SubmitField('Dodaj')
 
+class Grade(FlaskForm):
+    """formularz do wstawiania ocen"""
+    subject = SelectField('Wybierz przedmiot', choices=str)
+    term = RadioField('Wybierz semestr', choices=[('term1', 'Semestr 1'), ('term2', 'Semestr 2')])
+    category = SelectField('Kategoria ocen', choices=[('answer', 'Odpowiedź'), ('quiz', 'Kartkówka'), ('test', 'Sprawdzian')])
+    grade = SelectField('Ocena', choices=[
+        (6, 'Celujący'),
+        (5, 'Bardzo dobry'),
+        (4, 'Dobry'),
+        (3, 'Dostateczny'),
+        (2, 'Dopuszczający'),
+        (1, 'Niedostateczny')
+    ])
+    submit = SubmitField('Dodaj')
 
-def count_average(subject_value, term_value):
+def countAverage(subjectValue, termValue):
     """funkcja obliczająca średnie ocen"""
     with open('data/grades.json') as gradesFile:
         grades = json.load(gradesFile)
         gradesFile.close()
-    sum_grades = 0
+    sumGrades = 0
     lenght = 0
-    if subject_value == "" and term_value == "":
+    if subjectValue == "" and termValue == "":
         for subject, terms in grades.items():
             for term, categories in terms.items():
                 for category, grades in categories.items():
                     if category == 'answer' or category == 'quiz' or category == 'test':
                         for grade in grades:
-                            sum_grades += grade
+                            sumGrades += grade
                             lenght += 1
     else:
         for subject, terms in grades.items():
-            if subject == subject_value:
+            if subject == subjectValue:
                 for term, categories in terms.items():
-                    if term == term_value:
+                    if term == termValue:
                         for category, grades in categories.items():
                             if category == 'answer' or category == 'quiz' or category == 'test':
                                 for grade in grades:
-                                    sum_grades += grade
+                                    sumGrades += grade
                                     lenght += 1
     if lenght != 0:
-        return round(sum_grades / lenght, 2)
-
+        return round(sumGrades / lenght, 2)
 
 totalAverage = {}
-
-
-def yearly_average(subject_value, term_value):
+def yearlyAverage(subjectValue, termValue):
     with open('data/grades.json', encoding='utf-8') as gradesFile:
         grades = json.load(gradesFile)
         gradesFile.close()
-    sum_grades = 0
+    sumGrades = 0
     lenght = 0
-    if term_value == '':
+    if termValue == '':
         for subject, terms in grades.items():
-            if subject == subject_value:
+            if subject == subjectValue:
                 for term, categories in terms.items():
                     for category, grades in categories.items():
                         if category == 'answer' or category == 'quiz' or category == 'test':
                             for grade in grades:
-                                sum_grades += grade
+                                sumGrades += grade
                                 lenght += 1
-                                totalAverage[subject] = round(sum_grades / lenght, 2)
+                                totalAverage[subject] = round(sumGrades / lenght, 2)
     if lenght != 0:
-        return round(sum_grades / lenght, 2)
-
+        return round(sumGrades / lenght, 2)
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
-
 @app.route('/logIn', methods=['POST', 'GET'])
-def log_in():
+def logIn():
     login = LoginForm()
     with open('data/users.json') as usersFile:
         users = json.load(usersFile)
         usersFile.close()
     if login.validate_on_submit():
-        user_login = login.userLogin.data
-        user_pass = login.userPass.data
-        if user_login == users['userLogin'] and user_pass == users['userPass']:
-            session['userLogin'] = user_login
+        userLogin = login.userLogin.data
+        userPass = login.userPass.data
+        if userLogin == users['userLogin'] and userPass == users['userPass']:
+            session['userLogin'] = userLogin
             session['firstName'] = users['firstName']
             return redirect('dashboard')
     return render_template('login.html', title='Logowanie', login=login)
 
-
 @app.route('/logOut')
-def log_out():
+def logOut():
     session.pop('userLogin')
     session.pop('firstName')
-    return redirect('log_in')
-
+    return redirect('logIn')
 
 @app.route('/dashboard')
 def dashboard():
     with open('data/grades.json') as gradesFile:
         grades = json.load(gradesFile)
         gradesFile.close()
-    weather_source = requests.get('https://danepubliczne.imgw.pl/api/data/synop/station/krakow')
-    weather = json.loads(weather_source.content)
-    air_quality_source = requests.get('https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/400')
-    air_quality = json.loads(air_quality_source.content)
-    return render_template('dashboard.html', title='Dashboard', userLogin=session.get('userLogin'),
-                           firstName=session.get('firstName'), date=date, grades=grades, countAverage=count_average,
-                           yearlyAverage=yearly_average, weather=weather, airQuality=air_quality)
+    weatherSource = requests.get('https://danepubliczne.imgw.pl/api/data/synop/station/krakow')
+    weather = json.loads(weatherSource.content)
+    airQualitySource = requests.get('https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/400')
+    airQuality = json.loads(airQualitySource.content)
+    return render_template('dashboard.html', title='Dashboard', userLogin=session.get('userLogin'), firstName=session.get('firstName'), date=date, grades=grades, countAverage=countAverage, yearlyAverage=yearlyAverage, weather=weather, airQuality=airQuality)
 
+@app.route('/addSubject', methods=['POST', 'GET'])
+def addSubject():
+    subjectForm = Subject()
+    if subjectForm.validate_on_submit():
+        with open('data/grades.json', encoding='utf-8') as gradesFile:
+            grades = json.load(gradesFile)
+            gradesFile.close()
+            subject = subjectForm.subject.data
+            grades[subject] = {
+                'term1': {'answer': [], 'quiz': [], 'test': [], 'interim': 0},
+                'term2': {'answer': [], 'quiz': [], 'test': [], 'interim': 0, 'yearly': 0}
+            }
+        with open('data/grades.json', 'w', encoding='utf-8') as gradesFile:
+            json.dump(grades, gradesFile)
+            gradesFile.close()
+            flash('Przedmiot dodany poprawnie', 'success')
+            return redirect('dashboard')
+    return render_template('add-subject.html', title='Dodaj przedmiot', userLogin=session.get('userLogin'), firstName=session.get('firstName'), date=date, subjectForm=subjectForm)
+
+@app.route('/addGrade', methods=['POST', 'GET'])
+def addGrade():
+    gradeForm = Grade()
+    return render_template('add-grade.html', title='Dodaj ocenę', userLogin=session.get('userLogin'), firstName=session.get('firstName'), date=date, gradeForm=gradeForm)
 
 @app.errorhandler(404)
-def page_not_found():
+def pageNotFound(e):
     return render_template('404.html', title='Page not found'), 404
 
-
 @app.errorhandler(500)
-def server_error():
+def serverError(e):
     return render_template('500.html', title='Wewnętrzny błąd serwera :('), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 05-Users/app.py
+++ 05-Users/app.py
@@ -1,73 +1,110 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Length
+from flask_bcrypt import Bcrypt
+from flask_bs4 import Bootstrap
 import os
 
-from flask import Flask, render_template, redirect, url_for, flash
-from flask_bs4 import Bootstrap
-from flask_login import UserMixin, LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms.fields.simple import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
-
+# konfiguracja aplikacji
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'j4bng34limit89h(PHO#RP(H*f043hgo;b43n4o83r'
+app.config['SECRET_KEY'] = 'lkjh6789&^&*(OKJHG&*(*&YHJ'
 
+# konfiguracja bazy danych
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data/users.sqlite')
 db = SQLAlchemy(app)
-
-
+bcrypt = Bcrypt(app)
+# tabela w bazie danych
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(64))
-    user_lastname = db.Column(db.String(64))
-    user_email = db.Column(db.String(120), unique=True)
-    user_pass = db.Column(db.String(128))
+    firstName = db.Column(db.String(20))
+    lastName = db.Column(db.String(30))
+    userMail = db.Column(db.String(50), unique=True)
+    userPass = db.Column(db.String(50))
 
     def is_authenticated(self):
         return True
 
-
+# konfiguracja Flask-Login
 loginManager = LoginManager()
 loginManager.init_app(app)
 loginManager.login_view = 'login'
-loginManager.login_message = 'Please login to access this page.'
+loginManager.login_message = 'Nie jesteś zalogowany'
 loginManager.login_message_category = 'warning'
 
-
 @loginManager.user_loader
-def load_user(user_id):
-    return Users.query.filter_by(id=user_id).first()
+def loadUser(id):
+    return Users.query.filter_by(id=id).first()
 
-class LoginForm(FlaskForm):
-    user_email = EmailField('Email', validators=[DataRequired(), Email()])
-    user_pass = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+# formularze
+class Login(FlaskForm):
+    """formularz logowania"""
+    userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
+    userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
+    submit = SubmitField('Zaloguj')
 
+class Register(FlaskForm):
+    """formularz rejestracji"""
+    firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
+    lastName = StringField('Nazwisko', validators=[DataRequired()], render_kw={"placeholder": "Nazwisko"})
+    userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
+    userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
+    submit = SubmitField('Rejestruj')
 
-class RegisterForm(FlaskForm):
-    user_name = EmailField('Name', validators=[DataRequired()])
-    user_lastname = EmailField('Last Name', validators=[DataRequired()], render_kw={'placeholder': 'Last Name'})
-    user_email = EmailField('Email', validators=[DataRequired(), Email()])
-    user_pass = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-
+# główna aplikacja
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', headline='User Management')
-
+    return render_template('index.html', title='Home', headline='Zarządzanie użytkownikami')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-    return render_template('login.html', title='Sign In', login_form=login_form)
-
+    user = Users.query.all()
+    if not user:
+        return redirect(url_for('register'))
+    else:
+        loginForm = Login()
+        if loginForm.validate_on_submit():
+            user = Users.query.filter_by(userMail=loginForm.userMail.data).first()
+            if user:
+                if bcrypt.check_password_hash(user.userPass, loginForm.userPass.data):
+                    login_user(user)
+                    flash('Zostałeś zalogowany', 'success')
+                    return redirect(url_for('dashboard'))
+    return render_template('login.html', title='Logowanie', headline='Logowanie', loginForm=loginForm)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    register_form = RegisterForm()
-    return render_template('register.html', title='Register', register_form=register_form)
+    registerForm = Register()
+    if registerForm.validate_on_submit():
+        try:
+            hash_pass = bcrypt.generate_password_hash(registerForm.userPass.data)
+            new_user = Users(userMail=registerForm.userMail.data, userPass=hash_pass, firstName=registerForm.firstName.data, lastName=registerForm.lastName.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Zostałeś zarejestrowany', 'success')
+            return redirect(url_for('login'))
+        except Exception:
+            flash('Błąd rejestracji, ten email jest już zarejestrowany', 'danger')
+            return redirect(url_for('register'))
+    return render_template('register.html', title='Rejestracja', headline='Rejestracja', registerForm=registerForm)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Zostałeś wylogowany', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    users = Users.query.all()
+    return render_template('dashboard.html', title='Dashboard', users=users)
 
 
 if __name__ == '__main__':
