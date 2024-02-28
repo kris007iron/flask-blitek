7b25a3960b3ab542642d6c3eb12c82be9e3cc94c
kris007iron <kris007.iron@gmail.com>
2024-02-28 15:04:09 +0100

    owrkin on user edit

--- 05-Users/app.py
+++ 05-Users/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
-from sqlalchemy.testing.pickleable import User
 from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired, Length
 from flask_bcrypt import Bcrypt
 from flask_bs4 import Bootstrap
@@ -21,33 +21,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 db = SQLAlchemy(app)
 
 # tabela w bazie danych
+
+
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(20))
     lastName = db.Column(db.String(30))
     userMail = db.Column(db.String(50), unique=True)
     userPass = db.Column(db.String(50))
+    userRole = db.Column(db.String(20), default='user')
 
     def is_authenticated(self):
         return True
 
 # konfiguracja Flask-Login
+
+
 loginManager = LoginManager()
 loginManager.init_app(app)
 loginManager.login_view = 'login'
 loginManager.login_message = 'Nie jesteś zalogowany'
 loginManager.login_message_category = 'warning'
 
+
 @loginManager.user_loader
 def loadUser(id):
     return Users.query.filter_by(id=id).first()
 
 # formularze
+
+
 class Login(FlaskForm):
     """formularz logowania"""
     userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
     submit = SubmitField('Zaloguj')
+
 
 class Register(FlaskForm):
     """formularz rejestracji"""
@@ -57,17 +66,52 @@ class Register(FlaskForm):
     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
     submit = SubmitField('Rejestruj')
 
+
+class Add(FlaskForm):
+    """formularz dodawania nowego użytkownika"""
+    firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
+    lastName = StringField('Nazwisko', validators=[DataRequired()], render_kw={"placeholder": "Nazwisko"})
+    userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
+    userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
+    userRole = SelectField('Uprawnienia', validators=[DataRequired()], choices=[('user', 'Użytkownik'),
+                                                                                ('admin', 'Administrator')])
+    submit = SubmitField('Dodaj')
+
+
+class Edit(FlaskForm):
+    """formularz edycji użytkownika"""
+    firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
+    lastName = StringField('Nazwisko', validators=[DataRequired()],
+                           render_kw={"placeholder": "Nazwisko"})
+    userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
+    userRole = SelectField('Uprawnienia', validators=[DataRequired()],
+                           choices=[('user', 'Użytkownik'), ('admin', 'Administrator')])
+    submit = SubmitField('Edytuj')
+
+
+# class Password(FlaskForm):
+#     """formularz zmiany hasła użytkownika"""
+#     userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
+#     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
+#     submit = SubmitField('Zmień')
+
+
 class ChangeLoggedUser(FlaskForm):
     """formularz zmiany hasła zalogowanego użytkownika"""
     userEmail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
-    userPass = PasswordField('Hasło', validators=[DataRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Hasło"})
-    userNewPass = PasswordField('Nowe hasło', validators=[DataRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Nowe hasło"})
+    userPass = PasswordField('Hasło', validators=[DataRequired(), Length(min=8, max=20)],
+                             render_kw={"placeholder": "Hasło"})
+    userNewPass = PasswordField('Nowe hasło', validators=[DataRequired(), Length(min=8, max=20)],
+                                render_kw={"placeholder": "Nowe hasło"})
     submit = SubmitField('Zmień')
 
 # główna aplikacja
+
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Home', headline='Zarządzanie użytkownikami')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -84,13 +128,15 @@ def login():
                     return redirect(url_for('dashboard'))
     return render_template('login.html', title='Logowanie', headline='Logowanie', loginForm=loginForm)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     registerForm = Register()
     if registerForm.validate_on_submit():
         try:
             hashPass = bcrypt.generate_password_hash(registerForm.userPass.data)
-            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass, firstName=registerForm.firstName.data, lastName=registerForm.lastName.data)
+            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass,
+                            firstName=registerForm.firstName.data, lastName=registerForm.lastName.data)
             db.session.add(newUser)
             db.session.commit()
             flash('Konto utworzone poprawnie', 'success')
@@ -100,11 +146,13 @@ def register():
             return redirect(url_for('register'))
     return render_template('register.html', title='Rejestracja', headline='Rejestracja', registerForm=registerForm)
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/dashboard')
 @login_required
@@ -113,7 +161,9 @@ def dashboard():
     addUser = Register()
     editUser = Register()
     editUserPass = Register()
-    return render_template('dashboard.html', title='Dashboard', users=users, addUser=addUser, editUser=editUser, editUserPass=editUserPass)
+    return render_template('dashboard.html', title='Dashboard', users=users, addUser=addUser, editUser=editUser,
+                           editUserPass=editUserPass)
+
 
 @app.route('/add-user', methods=['GET', 'POST'])
 @login_required
@@ -122,7 +172,8 @@ def addUser():
     if addUser.validate_on_submit():
         try:
             hashPass = bcrypt.generate_password_hash(addUser.userPass.data)
-            newUser = Users(userMail=addUser.userMail.data, userPass=hashPass, firstName=addUser.firstName.data, lastName=addUser.lastName.data)
+            newUser = Users(userMail=addUser.userMail.data, userPass=hashPass, firstName=addUser.firstName.data,
+                            lastName=addUser.lastName.data)
             db.session.add(newUser)
             db.session.commit()
             flash('Użytkownik dodany poprawnie', 'success')
@@ -130,6 +181,7 @@ def addUser():
         except Exception:
             flash('Taki adres mail już istnieje, wpisz inny', 'danger')
             return redirect(url_for('dashboard'))
+
 
 @app.route('/edit-user<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -145,6 +197,7 @@ def editUser(id):
         flash('Dane zapisane poprawnie', 'success')
         return redirect(url_for('dashboard'))
 
+
 @app.route('/delete-user', methods=['GET', 'POST'])
 @login_required
 def deleteUser():
@@ -155,6 +208,7 @@ def deleteUser():
         db.session.commit()
         flash('Użytkownik usunięty poprawnie', 'success')
         return redirect(url_for('dashboard'))
+
 
 @app.route('/edit-user-pass', methods=['GET', 'POST'])
 @login_required
@@ -168,7 +222,7 @@ def editUserPass():
         return redirect(url_for('dashboard'))
 
 
-@app.route('/change' , methods=['GET', 'POST'])
+@app.route('/change', methods=['GET', 'POST'])
 @login_required
 def change():
     changeLoggedUser = ChangeLoggedUser()
@@ -181,6 +235,7 @@ def change():
                 flash('Hasło zmienione poprawnie', 'success')
                 return redirect(url_for('dashboard'))
     return render_template('change.html', changeLoggedUser=changeLoggedUser)
+
 
 if __name__ == '__main__':
     with app.app_context():
