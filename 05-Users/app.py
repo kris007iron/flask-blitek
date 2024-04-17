8b8e2678e5f98e68085e80ea0f0595473c9b192f
kris007iron <kris007.iron@gmail.com>
2024-04-17 14:55:07 +0200

    refactor

--- 05-Users/app.py
+++ 05-Users/app.py
@@ -19,7 +19,7 @@ app.config['SECRET_KEY'] = 'lkjh6789&^&*(OKJHG&*(*&YHJ'
 bcrypt = Bcrypt(app)
 app.config['UPLOAD_PATH'] = 'upload'
 app.config['UPLOAD_EXTENSIONS'] = ['.txt', '.png', '.jpg', '.jpeg']
-app.config['MAX_CONTENT_LENGHT'] = 16 * 1024 * 1024 # 16MB
+app.config['MAX_CONTENT_LENGHT'] = 16 * 1024 * 1024  # 16MB
 
 # konfiguracja bazy danych
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -27,6 +27,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 db = SQLAlchemy(app)
 
 # tabela w bazie danych
+
+
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(20))
@@ -38,12 +40,15 @@ class Users(db.Model, UserMixin):
     def is_authenticated(self):
         return True
 
+
 class Folders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     folderName = db.Column(db.String(50), unique=True)
     type = db.Column(db.String(20))
     icon = db.Column(db.String(20))
+
     time = db.Column(db.String(20))
+
 
 class Files(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -51,27 +56,36 @@ class Files(db.Model):
     type = db.Column(db.String(20))
     icon = db.Column(db.String(20))
     time = db.Column(db.String(20))
+
     size = db.Column(db.String(20))
 
 # konfiguracja Flask-Login
+
+
 loginManager = LoginManager()
 loginManager.init_app(app)
+
 loginManager.login_view = 'login'
 loginManager.login_message = 'Nie jesteś zalogowany'
 loginManager.login_message_category = 'warning'
+
 
 @loginManager.user_loader
 def loadUser(id):
     return Users.query.filter_by(id=id).first()
 
 # formularze
+
+
 class Login(FlaskForm):
     """formularz logowania"""
     userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
     submit = SubmitField('Zaloguj')
 
+
 class Register(FlaskForm):
+
     """formularz rejestracji"""
     firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
     lastName = StringField('Nazwisko', validators=[DataRequired()], render_kw={"placeholder": "Nazwisko"})
@@ -79,22 +93,27 @@ class Register(FlaskForm):
     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
     submit = SubmitField('Rejestruj')
 
+
 class Add(FlaskForm):
     """formularz dodawania użytkowników"""
     firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
     lastName = StringField('Nazwisko', validators=[DataRequired()], render_kw={"placeholder": "Nazwisko"})
     userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
     userPass = PasswordField('Hasło', validators=[DataRequired()], render_kw={"placeholder": "Hasło"})
-    userRole = SelectField('Uprawnienia', validators=[DataRequired()], choices=[('user', 'Użytkownik'), ('admin', 'Administrator')])
+    userRole = SelectField('Uprawnienia', validators=[DataRequired()],
+                           choices=[('user', 'Użytkownik'), ('admin', 'Administrator')])
     submit = SubmitField('Dodaj')
+
 
 class Edit(FlaskForm):
     """formularz edycji danych użytkowników"""
     firstName = StringField('Imię', validators=[DataRequired()], render_kw={"placeholder": "Imię"})
     lastName = StringField('Nazwisko', validators=[DataRequired()], render_kw={"placeholder": "Nazwisko"})
     userMail = EmailField('Mail', validators=[DataRequired()], render_kw={"placeholder": "Mail"})
-    userRole = SelectField('Uprawnienia', validators=[DataRequired()], choices=[('user', 'Użytkownik'), ('admin', 'Administrator')])
+    userRole = SelectField('Uprawnienia', validators=[DataRequired()],
+                           choices=[('user', 'Użytkownik'), ('admin', 'Administrator')])
     submit = SubmitField('Zapisz')
+
 
 class Password(FlaskForm):
     """formularz zmiany hasła przez użytkowników"""
@@ -103,39 +122,50 @@ class Password(FlaskForm):
     newUserPass = PasswordField('Nowe hasło', validators=[DataRequired()], render_kw={"placeholder": "Nowe hasło"})
     submit = SubmitField('Zapisz')
 
+
 class ChangePass(FlaskForm):
     """formularz do zmiany hasła uzytkownika z panelu admina"""
-    userPass = PasswordField('Nowe hasło:', validators=[DataRequired(), Length(min=3, max=50)], render_kw={"placeholder": "Nowe hasło"})
+    userPass = PasswordField('Nowe hasło:', validators=[DataRequired(), Length(min=3, max=50)],
+                             render_kw={"placeholder": "Nowe hasło"})
     submit = SubmitField('Zapisz')
+
 
 class Search(FlaskForm):
     """formularz wyszukiwania plików / folderów"""
     searchKey = StringField('Szukaj', validators=[DataRequired()])
     submit = SubmitField('Szukaj')
 
+
 class CreateFolders(FlaskForm):
     """formularz tworzenia nowego folderu"""
     folderName = StringField('Nazwa folderu', validators=[DataRequired()], render_kw={'placeholder': 'Nazwa folderu'})
     submit = SubmitField('Utwórz')
 
+
 class UploadFiles(FlaskForm):
     """formularz przesyłania plików"""
-    fileName = FileField('Plik', validators=[FileAllowed([app.config['UPLOAD_EXTENSIONS']])], render_kw={'placeholder': '.txt, .png, .jpg, .jpeg'})
+    fileName = FileField('Plik', validators=[FileAllowed([app.config['UPLOAD_EXTENSIONS']])],
+                         render_kw={'placeholder': '.txt, .png, .jpg, .jpeg'})
     submit = SubmitField('Prześlij')
+
 
 class renameFolders(FlaskForm):
     """formularz przesyłania plików"""
     folderName = StringField('Nazwa folderu', validators=[DataRequired()], render_kw={'placeholder': 'Nazwa folderu'})
     submit = SubmitField('Zapisz')
+
+
 class delFolders(FlaskForm):
     """formularz przesyłania plików"""
     submit = SubmitField('Usuń')
 
 # główna aplikacja
 
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Home', headline='Zarządzanie użytkownikami')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -152,6 +182,7 @@ def login():
                     return redirect(url_for('dashboard'))
     return render_template('login.html', title='Logowanie', headline='Logowanie', loginForm=loginForm)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     registerForm = Register()
@@ -159,7 +190,9 @@ def register():
     if registerForm.validate_on_submit() and not user:
         try:
             hashPass = bcrypt.generate_password_hash(registerForm.userPass.data)
-            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass, firstName=registerForm.firstName.data, lastName=registerForm.lastName.data, userRole='admin')
+            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass,
+                            firstName=registerForm.firstName.data,
+                            lastName=registerForm.lastName.data, userRole='admin')
             db.session.add(newUser)
             db.session.commit()
             flash('Konto utworzone poprawnie', 'success')
@@ -170,7 +203,9 @@ def register():
     elif registerForm.validate_on_submit():
         try:
             hashPass = bcrypt.generate_password_hash(registerForm.userPass.data)
-            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass, firstName=registerForm.firstName.data, lastName=registerForm.lastName.data, userRole='user')
+            newUser = Users(userMail=registerForm.userMail.data, userPass=hashPass,
+                            firstName=registerForm.firstName.data,
+                            lastName=registerForm.lastName.data, userRole='user')
             db.session.add(newUser)
             db.session.commit()
             flash('Konto utworzone poprawnie', 'success')
@@ -180,11 +215,13 @@ def register():
             # return redirect(url_for('register'))
     return render_template('register.html', title='Rejestracja', headline='Rejestracja', registerForm=registerForm)
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/dashboard')
 @login_required
@@ -201,7 +238,12 @@ def dashboard():
     folders = Folders.query.all()
     files = Files.query.all()
     editPass = Password()
-    return render_template('dashboard.html', editPass=editPass,delFolder=delFolder, renameFolder=renameFolder, title='Dashboard', users=users, addUser=addUser, editUser=editUser, editUserPass=editUserPass, search=search, createFolder=createFolder, uploadFile=uploadFile, files=files, folders=folders)
+    return render_template('dashboard.html', editPass=editPass,
+                           delFolder=delFolder, renameFolder=renameFolder,
+                           title='Dashboard', users=users, addUser=addUser, editUser=editUser,
+                           editUserPass=editUserPass, search=search, createFolder=createFolder,
+                           uploadFile=uploadFile, files=files, folders=folders)
+
 
 @app.route('/add-user', methods=['GET', 'POST'])
 @login_required
@@ -210,7 +252,8 @@ def addUser():
     if addUser.validate_on_submit():
         try:
             hashPass = bcrypt.generate_password_hash(addUser.userPass.data)
-            newUser = Users(userMail=addUser.userMail.data, userPass=hashPass, firstName=addUser.firstName.data, lastName=addUser.lastName.data, userRole=addUser.userRole.data)
+            newUser = Users(userMail=addUser.userMail.data, userPass=hashPass, firstName=addUser.firstName.data,
+                            lastName=addUser.lastName.data, userRole=addUser.userRole.data)
             db.session.add(newUser)
             db.session.commit()
             flash('Użytkownik dodany poprawnie', 'success')
@@ -218,6 +261,7 @@ def addUser():
         except Exception:
             flash('Taki adres mail już istnieje, wpisz inny', 'danger')
             return redirect(url_for('dashboard'))
+
 
 @app.route('/edit-user<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -233,6 +277,7 @@ def editUser(id):
         flash('Dane zapisane poprawnie', 'success')
         return redirect(url_for('dashboard'))
 
+
 @app.route('/delete-user', methods=['GET', 'POST'])
 @login_required
 def deleteUser():
@@ -243,6 +288,7 @@ def deleteUser():
         db.session.commit()
         flash('Użytkownik usunięty poprawnie', 'success')
         return redirect(url_for('dashboard'))
+
 
 @app.route('/edit-user-pass<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -255,23 +301,23 @@ def editUserPass(id):
         flash('Hasło zmienione poprawnie', 'success')
         return redirect(url_for('dashboard'))
 
+
 @app.route('/change-pass', methods=['GET', 'POST'])
 @login_required
 def changePass():
     changePassForm = Password()
     user = Users.query.filter_by(userMail=changePassForm.userMail.data).first()
     if changePassForm.validate_on_submit() and user:
-            if bcrypt.check_password_hash(user.userPass, changePassForm.userPass.data):
-                user.userPass = bcrypt.generate_password_hash(changePassForm.newUserPass.data)
-                flash('Hasło zostało zmienione pomyślnie', 'success')
-                db.session.commit()
-                return redirect(url_for('dashboard'))
-            else:
-                flash('Błąd przy zapisie hasła', 'danger')
+        if bcrypt.check_password_hash(user.userPass, changePassForm.userPass.data):
+            user.userPass = bcrypt.generate_password_hash(changePassForm.newUserPass.data)
+            flash('Hasło zostało zmienione pomyślnie', 'success')
+            db.session.commit()
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Błąd przy zapisie hasła', 'danger')
     else:
         flash('Błąd przy zapisie hasła', 'danger')
     return redirect(url_for('dashboard'))
-
 
 
 @app.route('/upload-file', methods=['GET', 'POST'])
@@ -306,6 +352,7 @@ def uploadFile():
         flash('Plik przesłany poprawnie', 'success')
         return redirect(url_for('dashboard'))
 
+
 @app.route('/rename-file<int:id>', methods=['GET', 'POST'])
 @login_required
 def renameFile(id):
@@ -318,19 +365,19 @@ def renameFile(id):
         print(arr)
         if len(arr) == 2:
             if not arr[1] == fileToRename.type:
-                newFileName = arr[0] +  f".{fileToRename.type}"
+                newFileName = arr[0] + f".{fileToRename.type}"
                 flash(f'Pomyślnie zmieniono nazwę pliku {fileToRename.fileName} na {newFileName}', 'success')
         else:
-            newFileName += arr[0] +  f".{fileToRename.type}"
+            newFileName += arr[0] + f".{fileToRename.type}"
             flash(f'Zmieniono nazwę pliku {fileToRename.fileName} na {newFileName}', 'success')
         print(newFileName)
         # if not newFileName.split(".")[1]
-        os.rename(os.path.join(app.config['UPLOAD_PATH'], fileToRename.fileName), os.path.join(app.config['UPLOAD_PATH'], newFileName))
+        os.rename(os.path.join(app.config['UPLOAD_PATH'], fileToRename.fileName),
+                  os.path.join(app.config['UPLOAD_PATH'], newFileName))
         fileToRename.fileName = newFileName
         db.session.commit()
 
     return redirect(url_for('dashboard'))
-
 
 
 @app.route('/delete-file<int:id>', methods=['GET', 'POST'])
@@ -357,6 +404,7 @@ def createFolder():
         flash('Folder utworzony poprawnie', 'success')
         return redirect(url_for('dashboard'))
 
+
 @app.route('/rename-folder<int:id>', methods=['GET', 'POST'])
 @login_required
 def renameFolder(id):
@@ -366,7 +414,8 @@ def renameFolder(id):
         print(newFolderName)
         folder = Folders.query.filter_by(id=id).first()
         if folder:
-            os.rename(os.path.join(app.config['UPLOAD_PATH'], folder.folderName), os.path.join(app.config['UPLOAD_PATH'], newFolderName))
+            os.rename(os.path.join(app.config['UPLOAD_PATH'], folder.folderName),
+                      os.path.join(app.config['UPLOAD_PATH'], newFolderName))
             folder.folderName = newFolderName
         db.session.commit()
         flash(f'Pomyślnie zmieniono nazwę foldery {id} na {newFolderName}', 'success')
@@ -382,6 +431,7 @@ def renameFolder(id):
     # db.session.commit()
     return redirect(url_for('dashboard'))
 
+
 @app.route('/delete-folder<int:id>', methods=['GET', 'POST'])
 @login_required
 def deleteFolder(id):
@@ -391,6 +441,7 @@ def deleteFolder(id):
     db.session.commit()
     flash(f'Pomyślnie usunięto folder {folderToDelete.folderName}', 'success')
     return redirect(url_for('dashboard'))
+
 
 if __name__ == '__main__':
     with app.app_context():
